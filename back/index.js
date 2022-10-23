02df57f08fd97224facb65fc54b84404dfa4aee5
dongmin7208 <sayg3o3d@gmail.com>
2022-10-23 11:25:05 +0900

    compose

--- back/index.js
+++ back/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001;
+const cors = require("cors")
+
+app.use(cors());
+//모든걸 허용
+//sever to server cors필요없음
+//server to 브라우저에서만
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.json({ mas: 'This is CORS-enabled for all origins!' })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
